3d41893fa6beccae9134cd4d2b774df725a03b3a
LaRojaPlamya162 <tuananhls16022004@gmail.com>
2025-12-15 16:27:08 +0700

    End-to-end episode completed

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -12,30 +12,36 @@ import torch
 import numpy as np
 import os
 import csv
+import random
 from ament_index_python.packages import get_package_share_directory
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 from sensor_msgs.msg import JointState
 from tf2_ros import Buffer, TransformListener
 from gazebo_msgs.srv import SetEntityState
 from gazebo_msgs.msg import EntityState
-import os
-class BCController(Node):
+class Controller(Node):
     def __init__(self, node_name, model_name):
         super().__init__(node_name)
-
         package_share_dir = get_package_share_directory('controller')
         model_path = os.path.join(package_share_dir, model_name)
+        
         # ===== Info =====
-        self.intital_ur5_pose = None
-        self.intital_ball_pose = None
-        self.done = []
+        
+        self.done_flag = False
         self.resetting = False
+        self.episode_step = 0
+        self.episode = 0
+        self.timestep = 0
+        self.new_episode_ready = True
+
         # ===== Model =====
+        
         self.model = BCPolicy(state_dim=6, action_dim=6)
         self.model.load_state_dict(torch.load(model_path, weights_only=True))
         self.model.eval()
 
         # ===== Subscriptions =====
+        
         self.ball_name = "cricket_ball"
         self.ball_sub = self.create_subscription(
             Odometry,
@@ -52,6 +58,7 @@ class BCController(Node):
         )
 
         # ===== Publisher =====
+        
         self.pub = self.create_publisher(
             JointTrajectory,
             '/joint_trajectory_controller/joint_trajectory',
@@ -66,7 +73,9 @@ class BCController(Node):
             "wrist_2_joint",
             "wrist_3_joint"
         ]
+        
         # ====== Delete client =====
+        
         self.delete_client = self.create_client(
             DeleteEntity,
             '/delete_entity'
@@ -78,25 +87,33 @@ class BCController(Node):
             '/spawn_entity'
         )
         # ===== State =====
+        
         self.last_joint_state = None
         self.ball_pos = None   # (x, y, z)
         self.joint_pos = None
+        self.intial_ur5_pose = None
+        self.intial_ball_pose = None
+
         # ===== Transpose ======
+        
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
         # ===== Logging =====
+        
         self.timestep = 0
-        self.csv = open("bc_log_v3.csv", "w", newline="")
+        self.episode = 0
+        self.csv = open("bc_log_v4.csv", "w", newline="")
         self.writer = csv.writer(self.csv)
         self.writer.writerow([
             "timestep",
+            "episode",
             "s1","s2","s3","s4","s5","s6",
             "a1","a2","a3","a4","a5","a6",
             "ball_pos_x","ball_pos_y","ball_pos_z",
             "wrist_3_x", "wrist_3_y", "wrist_3_z",
             "reward",
-            "done"
+            "done",
         ])
 
         # ===== Timer =====
@@ -124,19 +141,23 @@ class BCController(Node):
 
         # Angular velocity
 
-        self.get_logger().info(
+        """self.get_logger().info(
             f"Pos: [{x:.3f}, {y:.3f}, {z:.3f}] | "
-        )
+        )"""
     def control_step(self):
+        # ===== Condition rules =====
+        if self.resetting or not self.new_episode_ready:
+            return
         if self.last_joint_state is None or self.ball_pos is None:
-            self.get_logger().info(f"Error")
+            #self.get_logger().info(f"Error")
             return
-        if not self.tf_buffer.can_transform("base_link", "wrist_3_link", rclpy.time.Time()):
+        if not self.tf_buffer.can_transform("base_link", "wrist_3_link", rclpy.time.Time(seconds = 0)):
             return
-        """if len(self.done) > 0 and self.done[-1] and not self.resetting:
+        if self.done_flag:
             self.resetting = True
+            self.new_episode_ready = False
             self.reset_episode()
-            return"""
+            return
         x,y,z = np.nan, np.nan, np.nan
         
         # ===== Model prediction =====
@@ -184,88 +205,121 @@ class BCController(Node):
         
         # ===== Log =====
         self.joint_pos = [x,y,z]
-        reward_fn = RewardFunction(self.ball_pos, self.joint_pos, self.timestep)
+        reward_fn = RewardFunction(self.ball_pos, self.joint_pos, self.episode_step)
         reward = reward_fn.reward
-        done = reward_fn.done
-        self.done.append(done)
-        """if self.timestep == 0:
+        self.done_flag = reward_fn.done
+        if self.intial_ur5_pose is None and self.last_joint_state is not None:
+            self.intial_ur5_pose = obs.tolist()
+            self.get_logger().info("Initial UR5 pose captured")
+
+        """if self.timestep == 0 :
             self.intial_ur5_pose = obs.tolist()
             self.intial_ball_pose = self.ball_pos.copy()"""
+            #self.get_logger().info(self.intial_ball_pose)
         row = (
-        [self.timestep] +
-        current_pos.tolist()
+        [self.timestep]
+        + [self.episode] 
+        + current_pos.tolist()
         #current_pos.numpy().tolist()
         + action.tolist()
         + self.ball_pos
         + self.joint_pos
         + [reward]
-        + [done]
+        + [self.done_flag]
         )
         self.writer.writerow(row)
-        self.get_logger().info(f"Sent action: {action}")
+        #self.get_logger().info(f"Sent action: {action}")
         self.timestep += 1 
+        self.episode_step += 1
+    
+    # ===== End of episode reset ====
 
     def reset_episode(self):
+        self.get_logger().info(f"Resetting episode {self.episode + 1}")
+        self.done_flag = False
+        self.episode += 1
+        self.episode_step = 0
+        self.new_episode_ready = False
         self.reset_ur5_pose()
-        while not self.delete_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for /delete_entity...')
 
-        while not self.respawn_client.wait_for_service(timeout_sec=1.0):
-            self.get_logger().info('Waiting for /spawn_entity...')
-
+        #rclpy.spin_once(self, timeout_sec=2.0)
+        self.get_logger().info(f"Start respawning ball pose")
         self.reset_ball_pose()
+    def reset_ur5_pose(self):
+        if self.intial_ur5_pose is None:
+            return
+        traj = JointTrajectory()
+        traj.joint_names = self.joint_names
+        
+        point = JointTrajectoryPoint()
+        point.positions = self.intial_ur5_pose
+        point.time_from_start.sec = 2  
+
+        traj.points.append(point)
+        self.pub.publish(traj)
+        #self.new_episode_ready = False
+
+    def is_ur5_at_initial_pose(self, tol=1e-2):
+        if self.last_joint_state is None:
+            return False
+
+        name_to_pos = dict(zip(
+            self.last_joint_state.name,
+            self.last_joint_state.position
+        ))
+        current = np.array([name_to_pos[j] for j in self.joint_names])
+        target = np.array(self.intial_ur5_pose)
+
+        return np.allclose(current, target, atol=tol)
+    
     def reset_ball_pose(self):
         # ===== Delete ball =====
-        req = DeleteEntity.Request()
-        req.name = 'cricket_ball'
-        #self.delete_client.call_async(req)
-        future = self.delete_client.call_async(req)
-        #rclpy.spin_until_future_complete(self, future)
+        delete_req = DeleteEntity.Request()
+        delete_req.name = 'cricket_ball'
+        self.get_logger().info(f"Deleting ball...")
+        future = self.delete_client.call_async(delete_req)
         future.add_done_callback(self._on_delete_done)
-
-        # ===== Spawn ball ====
-        req = SpawnEntity.Request()
-        req.name = 'cricket_ball'
+       
+    def _on_delete_done(self, future):
+        self.get_logger().info("Spawning ball...")
+        spawn_req = SpawnEntity.Request()
+        spawn_req.name = 'cricket_ball'
         model_path = os.path.expanduser(
         '~/.gazebo/models/cricket_ball/model.sdf'
         )
 
-        with open(model_path, 'r') as f:
-            req.xml = f.read()
+        with open(model_path) as f:
+            spawn_req.xml = f.read()
 
         pose = Pose()
-        pose.position.x = 0.0
-        pose.position.y = 0.0
+        pose.position.x = random.uniform(0.1, 0.7)
+        pose.position.y = random.uniform(-0.4, 0.4)
         pose.position.z = 0.414
         pose.orientation.w = 1.0
 
-        req.intial_pose = pose
-        req.reference_frame = 'world'
-        #self.ball_pos = self.intial_ball_pose.copy()
-        #self.respawn_client.call_async(req)
-        future = self.respawn_client.call_async(req)
+        spawn_req.initial_pose = pose
+        spawn_req.reference_frame = 'world'
+
+        future = self.respawn_client.call_async(spawn_req)
         future.add_done_callback(self._on_spawn_done)
-        return
-    def reset_ur5_pose(self):
-        traj = JointTrajectory()
-        traj.joint_names = self.joint_names
 
-        point = JointTrajectoryPoint()
-        point.positions = self.intial_ur5_pose
-        point.time_from_start.sec = 2  # reset chậm, an toàn
-
-        traj.points.append(point)
-        self.pub.publish(traj)
-
-        self.timestep = 0
-
-        self.done = []
     def _on_spawn_done(self, future):
-        self.resetting = False
-        self.get_logger().info("Ball respawned")
+        self.get_logger().info("Ball spawned, waiting for UR5...")
+        def wait_timer():
+            if self.is_ur5_at_initial_pose():
+                self.get_logger().info("UR5 ready, start new episode")
+                self.resetting = False
+                self.new_episode_ready = True
+                self.destroy_timer(timer)
+                
+
+        timer = self.create_timer(0.1, wait_timer)
+
+    
+
 def main(args=None):
     rclpy.init(args=args)
-    node = BCController('bc_controller', 'bc_model_v2.pth')
+    node = Controller('bc_controller', 'bc_model_v2.pth')
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
--- src/controller/controller/reward.py
+++ src/controller/controller/reward.py
@@ -1,19 +1,24 @@
 import math
 class RewardFunction():
-    def __init__(self,ball_pos, joint_pos, timestep):
+    def __init__(self,ball_pos, joint_pos, episode):
         super().__init__()
         self.joint_pos = joint_pos
         self.ball_pos = ball_pos
-        self.timestep = timestep
-        self.done = check_if_ball_out_of_table(self.ball_pos)
-        self.reward = reward(self.ball_pos, self.joint_pos, check_if_ball_out_of_table(self.ball_pos), self.timestep)
-        #print(self.reward)
+        self.episode = episode
+        self.done = check_if_done(self.ball_pos, self.episode)
+        self.reward = reward(self.ball_pos, self.joint_pos, self.done, self.episode)
+
 def euclidean_distance(a, b):
     return math.sqrt(sum((x - y) ** 2 for x, y in zip(a, b)))
+
 def distance_to_nearest_edge(ball_pos):
     return min(abs(ball_pos[0] - 0), abs(0.8 - ball_pos[0]), abs(ball_pos[1] - (-0.4)), abs(0.4 - ball_pos[1]))
-def check_if_ball_out_of_table(ball_pos):
-    if(ball_pos[2] < 0.414 or ball_pos[0] > 0.8 or ball_pos[0] < 0 or ball_pos[1] > 0.4 or ball_pos[1] < -0.4):
+
+def check_if_done(ball_pos, episode):
+    
+    if(ball_pos[2] < 0.45 or ball_pos[0] > 0.8 or ball_pos[0] < 0 or ball_pos[1] > 0.4 or ball_pos[1] < -0.4): # ball out of table
+        return True
+    elif episode == 2000: # in the table, timelimit = 2000
         return True
     return False
 
